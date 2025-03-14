44694d76c48715ed485bde6a013bf35acce911a4
Muqri Andri <23FTT1120@student.pb.edu.bn>
2025-03-15 06:44:02 +0800

    Implemented Elastic IP

--- config/app.php
+++ config/app.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'base_url' => 'http://47.129.37.22/enrichment-point-passport',
+    'base_url' => 'http://52.221.56.103/enrichment-point-passport',
     'protected_pages' => ['dashboard', 'profile', 'cca'],
     'default_page' => 'login',
     'path_prefix' => '/enrichment-point-passport'
